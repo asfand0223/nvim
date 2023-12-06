928b8a696e4e8fd173015d0120782cc7a48f06f1
Asfand Ali <asfand0223@gmail.com>
2023-12-06 18:07:02 +0000

    removed duplicated config folder that was hidden

--- .config/nvim/lua/core/options.lua
+++
@@ -1,3 +0,0 @@
-vim.g.mapleader = " "
-vim.wo.relativenumber = true
-vim.opt.shiftwidth = 4
--- .config/nvim/lua/plugins/ionide.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "ionide/Ionide-vim",
-    config = function()
-        local ionide = require("ionide")
-        ionide.setup({})
-    end,
-}
