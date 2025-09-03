11f7a2193774b696c3e7eaaaed03afca06025cba
asfand0223 <asfand0223@gmail.com>
2025-09-03 07:49:41 +0100

    Fix LSPs

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -4,3 +4,4 @@ vim.opt.shiftwidth = 4
 vim.opt.termguicolors = true
 vim.opt.guicursor = "n-v-i-c:block-Cursor"
 vim.opt.clipboard = "unnamedplus"
+vim.opt.background = "dark"
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,53 +5,9 @@ return {
 		local lualine = require("lualine")
 		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
-		local colors = {
-			purple = "#2308FC",
-			grey = "#656565",
-			green = "#11FF00",
-			red = "#FF4A4A",
-			white = "#FFFFFF",
-			fg = "#FFFFFF",
-			bg = "#2308FC",
-			inactive_bg = "#2c3043",
-		}
-
-		local my_lualine_theme = {
-			normal = {
-				a = { bg = colors.purple, fg = colors.fg, gui = "bold" },
-				b = { bg = colors.fg, fg = colors.bg },
-				c = { bg = colors.fg, fg = colors.bg },
-			},
-			insert = {
-				a = { bg = colors.white, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			visual = {
-				a = { bg = colors.grey, fg = colors.fg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			command = {
-				a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			replace = {
-				a = { bg = colors.red, fg = colors.fg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			inactive = {
-				a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
-				b = { bg = colors.inactive_bg, fg = colors.semilightgray },
-				c = { bg = colors.inactive_bg, fg = colors.semilightgray },
-			},
-		}
-
 		lualine.setup({
 			options = {
-				theme = my_lualine_theme,
+				theme = "dracula-nvim",
 			},
 			sections = {
 				lualine_x = {
